680d38355b7f0e6ee15378daec3b923b7354d5a8
Vachounet <vachounet@live.fr>
2017-01-25 17:02:02 +0100

    griffin: fix sensor config files path

--- device.mk
+++ device.mk
@@ -195,8 +195,8 @@ PRODUCT_PACKAGES += \
     sensors.msm8996
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
-    $(LOCAL_PATH)/configs/hals.conf:system/etc/sensors/hals.conf 
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf 
 
 PRODUCT_PACKAGES += \
     ipacm \
