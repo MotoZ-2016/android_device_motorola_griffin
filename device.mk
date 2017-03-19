a38e06b66da5c6087c4bd59c108c3be45caf10da
Vachounet <vachounet@live.fr>
2017-03-19 10:13:14 +0100

    Revert "griffin: use prebuild audio HAL"
    * Only way to fix mic in speaker mode while in call atm
    * Should help to support audio Mods
    
    (reverted from commit 95d9238eecc4b676d5f1a31515f7f62e9e96e9b5)

--- device.mk
+++ device.mk
@@ -83,6 +83,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audiod \
     audio.a2dp.default \
+    audio.primary.msm8996 \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
@@ -90,6 +91,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcomvoiceprocessingdescriptors \
+    libtinyalsa \
     libtinycompress \
     libtinyxml \
     tinymix
