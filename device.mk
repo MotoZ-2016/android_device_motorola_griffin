25d5f23bb69d85dd1e05d95154fa6a102b953994
Kevin Rocard <krocard@google.com>
2017-08-17 12:14:53 -0700

    griffin: audio: Convert audio_effects.conf to XML
    Converted .conf format to .xml using:
    https://github.com/luk1337/aeffects-conf2xml
    
    Bug: 37492580
    Test: dumped the effectFactory state and check it was unchanged
          $ adb shell dumpsys media.audio_flinger | sed '/Clients:/q'
    Change-Id: I233a44c2ba0cb4c4d4fb8c3720fc09862ce79827
    Merged-In: I233a44c2ba0cb4c4d4fb8c3720fc09862ce79827
    Signed-off-by: Kevin Rocard <krocard@google.com>
    
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -108,7 +108,7 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
