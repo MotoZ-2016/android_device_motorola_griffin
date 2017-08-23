35a97efc35820e90c2ecb7d48a697580734f492e
Erfan Abdi <erfangplus@gmail.com>
2017-08-23 16:46:23 +0300

    griffin: fix sensor shims building error
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -21,7 +21,7 @@ LOCAL_SRC_FILES := \
     SensorManager.cpp \
     moto_camera_misc.c
 
-LOCAL_SHARED_LIBRARIES := libutils libgui liblog libbinder
+LOCAL_SHARED_LIBRARIES := libutils libgui liblog libbinder libsensor
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
