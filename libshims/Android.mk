dff44b384763b0b273ec61312ae4b564593fcd59
Erfan Abdi <erfangplus@gmail.com>
2017-08-24 11:58:39 +0300

    griffin: move to O [2]
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
    
    # Conflicts:
    #	rootdir/etc/init.qcom.rc

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -26,3 +26,12 @@ LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := get_process_name.c
+
+LOCAL_MODULE := libshims_get_process_name
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
