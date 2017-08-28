0aefe6c40aa35ca220fcd94bd413697afcb968c2
Erfan Abdi <erfangplus@gmail.com>
2017-08-28 18:25:35 +0300

    griffin: remove hostapd
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -335,16 +335,12 @@ PRODUCT_PACKAGES += \
     wificond \
     wifilogd \
     libwpa_client \
-    hostapd \
     dhcpcd.conf \
     wpa_supplicant \
     wpa_supplicant.conf \
     wifi_symlinks
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
