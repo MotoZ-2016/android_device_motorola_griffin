5fb4fb5c63864a3253563651f73b4d208f8b7791
Alberto97 <albertop2197@gmail.com>
2017-12-10 18:12:20 +0100

    griffin: Update Moto RIL shim for Oreo
    Change-Id: If22b0c2febc9616699df47e19d19764b80b19a8b

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -25,8 +25,9 @@ include $(BUILD_SHARED_LIBRARY)
 
 # RIL
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := sensor.cpp
-LOCAL_SHARED_LIBRARIES := libsensor
+LOCAL_SRC_FILES := ASensorManager.cpp
+LOCAL_SHARED_LIBRARIES := android.hardware.sensors@1.0
+LOCAL_STATIC_LIBRARIES := android.hardware.sensors@1.0-convert
 LOCAL_MODULE := libshim_ril
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
