8b2b744cd5012bf308fc90128d78bdd1ac354eea
Shreps <shr3ps@gmail.com>
2018-01-04 14:48:00 +0100

    griffin: update lineage.mk
    Change-Id: I58e2936495a85111b7a8c669d4c839bcc113e19d

--- lineage.mk
+++ lineage.mk
@@ -26,10 +26,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Time Zone data for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:root/system/usr/share/zoneinfo/tzdata
-
 DEVICE_PACKAGE_OVERLAYS += device/motorola/griffin/overlay
 
 # Boot animation
@@ -43,14 +39,12 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_NAME := lineage_griffin
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := griffin
-PRODUCT_MODEL := XT1650
+PRODUCT_MODEL := Moto Z
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 TARGET_VENDOR_PRODUCT_NAME := griffin_retail
 TARGET_VENDOR_DEVICE_NAME := griffin
-
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=griffin PRODUCT_NAME=griffin
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=motorola/griffin/griffin:7.1.1/NPL26.118-20/19:user/release-keys \
