61de034baf4ab7ea7937c3c193f029640d0f24c1
Sean McCreary <mccreary@mcwest.org>
2018-01-27 19:01:46 -0700

    griffin: define protectionLevel for Mod permissions
    Add a module to assign 'signatureOrSystem' protectionLevel for
    Motorola's custom Mod permissions
    
    Change-Id: Ieb1db64615b5143cfa785426cd4f1003e5bfad5b
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -261,6 +261,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Mods
+PRODUCT_PACKAGES += \
+    ModPermsFix
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
