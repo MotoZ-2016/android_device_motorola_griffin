1aaeb59ba6863f73540497b3a1f8953a4f62006d
Erfan Abdi <erfangplus@gmail.com>
2018-02-24 16:55:07 +0300

    griffin: Switch to lineage vibrator HAL
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -387,8 +387,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.lineage
 
 # VR
 PRODUCT_PACKAGES += \
