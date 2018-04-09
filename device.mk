798f7e2e819014f340fdf23b76ef222c5c2fd418
Erfan Abdi <erfangplus@gmail.com>
2018-04-09 14:18:17 +0300

    griffin: Build SDM livedisplay HAL
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -251,7 +251,11 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-qdcm
+    vendor.lineage.livedisplay@1.0-service-sdm \
+    vendor.lineage.livedisplay-V1.0-java
+
+PRODUCT_BOOT_JARS += \
+    vendor.lineage.livedisplay-V1.0-java
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
