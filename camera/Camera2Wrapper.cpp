2c246f9ce2db33015ed9f3e5c656f59a1324d697
Demon000 <demonsingur@gmail.com>
2018-05-21 11:20:24 +0200

    griffin: camera: store user pointer and pass it when needed
    The new camera interface calls set_callbacks with a pointer
    to it's internal CameraDevice and expects following calls to
    the callbacks to use that pointer.
    
    Store the pointer in the camera wraper and intercept the
    callbacks calls to pass it along.
    
    Change-Id: I99f02484e12a3f72cf1be13f1c724f474a452d7f
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- camera/Camera2Wrapper.cpp
+++ camera/Camera2Wrapper.cpp
@@ -39,6 +39,12 @@ typedef struct wrapper_camera2_device {
 
 static camera_module_t *gVendorModule = 0;
 
+static camera_notify_callback gUserNotifyCb = NULL;
+static camera_data_callback gUserDataCb = NULL;
+static camera_data_timestamp_callback gUserDataCbTimestamp = NULL;
+static camera_request_memory gUserGetMemory = NULL;
+static void *gUserCameraDevice = NULL;
+
 static int check_vendor_module()
 {
     int rv = 0;
@@ -110,6 +116,25 @@ static int camera2_set_preview_window(struct camera_device * device,
     return VENDOR_CALL(device, set_preview_window, window);
 }
 
+void camera_notify_cb(int32_t msg_type, int32_t ext1, int32_t ext2, void *user) {
+    gUserNotifyCb(msg_type, ext1, ext2, gUserCameraDevice);
+}
+
+void camera_data_cb(int32_t msg_type, const camera_memory_t *data, unsigned int index,
+        camera_frame_metadata_t *metadata, void *user) {
+    gUserDataCb(msg_type, data, index, metadata, gUserCameraDevice);
+}
+
+void camera_data_cb_timestamp(nsecs_t timestamp, int32_t msg_type,
+        const camera_memory_t *data, unsigned index, void *user) {
+    gUserDataCbTimestamp(timestamp, msg_type, data, index, gUserCameraDevice);
+}
+
+camera_memory_t* camera_get_memory(int fd, size_t buf_size,
+        uint_t num_bufs, void *user) {
+    return gUserGetMemory(fd, buf_size, num_bufs, gUserCameraDevice);
+}
+
 static void camera2_set_callbacks(struct camera_device * device,
         camera_notify_callback notify_cb,
         camera_data_callback data_cb,
@@ -122,7 +147,14 @@ static void camera2_set_callbacks(struct camera_device * device,
     if(!device)
         return;
 
-    VENDOR_CALL(device, set_callbacks, notify_cb, data_cb, data_cb_timestamp, get_memory, user);
+    gUserNotifyCb = notify_cb;
+    gUserDataCb = data_cb;
+    gUserDataCbTimestamp = data_cb_timestamp;
+    gUserGetMemory = get_memory;
+    gUserCameraDevice = user;
+
+    VENDOR_CALL(device, set_callbacks, camera_notify_cb, camera_data_cb,
+            camera_data_cb_timestamp, camera_get_memory, user);
 }
 
 static void camera2_enable_msg_type(struct camera_device * device, int32_t msg_type)
