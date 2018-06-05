03704b2a6e51530dfc58b85fb08f22908fff0511
Brandon <bsolis@codeaurora.org>
2018-06-05 09:25:27 -0700

    griffin: New HAL for passing SIP call info headers to clients
    Desc: Permission to access the QMI and HAL from RCS Daemon
    
    Change-Id: I45d944376179d0f6f6704563ac3da631a3fa9c64

--- fs_config/file_caps.fs
+++ fs_config/file_caps.fs
@@ -88,6 +88,18 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
