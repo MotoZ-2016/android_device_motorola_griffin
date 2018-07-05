4c7858e1d4885bbf4ddf5fed58d47acfcbd7f871
Erfan Abdi <erfangplus@gmail.com>
2018-07-06 00:49:48 +0430

    griffin: compile kernel with clang
    Change-Id: I717636855e97a43fe30a9d26835888f5494dfde5
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := griffin_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8996
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_griffin
