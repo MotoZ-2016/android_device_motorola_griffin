6dae9967acb2887ac712c8aa8bde19c3c2d12529
Erfan Abdi <erfangplus@gmail.com>
2018-09-28 13:30:32 +0300

    griffin: upver to power hal 1.1
    Change-Id: I9adb5b5f807ea13ca9644105f9f83c380cf9337a
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -300,7 +300,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti \
+    android.hardware.power@1.1-service-qti \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
