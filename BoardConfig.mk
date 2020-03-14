a4b6c3bfd53ca8e3662d55902342b99b036bf260
Erfan Abdi <erfangplus@gmail.com>
2020-03-14 11:40:12 +0330

    griffin: Add missing kernel cmdline
    Change-Id: I9838371f5f9ab32fc84b0bba28b0069623c3e7a5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,6 +114,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 BOARD_KERNEL_BASE := 0x80000000
 #BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
+BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1 app_setting.use_32bit_app_setting=1 kpti=1
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += cnsscore.pcie_link_down_panic=1
 BOARD_KERNEL_PAGESIZE := 4096
