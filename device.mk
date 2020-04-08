e185edb584f1fe578eaa7f8cd60df263a228bea0
Roman Kiryanov <rkir@google.com>
2020-04-08 14:23:47 -0700

    griffin: Replace audio@2.0-service with audio.service
    Legacy service name, use android.hardware.audio.service instead
    
    Bug: 153579444
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Merged-In: I4c05accb86dd0c9cd1b3fef3d84e0d54b2afc6a1
    Change-Id: I6b55b4b1db2577f4176c99dca7021cfe78e13307

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio.service \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.0-impl
