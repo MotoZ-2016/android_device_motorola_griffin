448cd6585b028b08eb21839bce2198696cc6cb27
Erfan Abdi <erfangplus@gmail.com>
2020-06-27 16:09:25 +0430

    griffin: Cleanup bluetooth blobs
    * And build vendor.qti.hardware.btconfigstore@1.0 from source
    
    Change-Id: I89533bac6af3f685c64e61f4f7c2ad9a52d26be6

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+
 # LineageActions
 PRODUCT_PACKAGES += \
     LineageActions
