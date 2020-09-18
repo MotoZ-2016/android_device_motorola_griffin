374c3cd4337857f4c1b98836f63d0a1b7a7a4062
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    griffin: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,7 +206,7 @@ BOARD_VNDK_VERSION := current
 BOARD_NFC_CHIPSET := pn544
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy-mods/vendor
