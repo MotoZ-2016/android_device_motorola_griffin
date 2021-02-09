5d9c7a1f0690cca48f44d7bae8a1c6bc94102c4d
Erfan Abdi <erfangplus@gmail.com>
2021-02-09 16:35:08 +0330

    griffin: Separate thermal and power sections
    Change-Id: If03f385ddbb4379a5ac265b46e4e8ce29d647db8

--- device.mk
+++ device.mk
@@ -322,10 +322,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.0.vendor \
-    thermal.msm8996 \
-    android.hardware.thermal@1.0-impl:64 \
-    android.hardware.thermal@1.0-service
+    vendor.qti.hardware.perf@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
@@ -417,6 +414,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
+
+# Thermal
+PRODUCT_PACKAGES += \
+    thermal.msm8996 \
+    android.hardware.thermal@1.0-impl:64 \
+    android.hardware.thermal@1.0-service
 
 # Trust HAL
 PRODUCT_PACKAGES += \
